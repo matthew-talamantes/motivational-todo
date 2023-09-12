74b720f38e8357aa0aa04f8e40f02caa61b5d14b
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2023-09-12 15:37:47 -0400

    Move Header to layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,7 @@
-import Header from '@/components/header';
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className="">
-      </main>
-    </>
+    <main className="">
+    </main>
   )
 }
