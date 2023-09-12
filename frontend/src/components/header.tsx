a1d220f9196172c1f03bb0ef1b464cdc676c8d80
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2023-09-12 16:37:32 -0400

    Seperate navItems into their own component

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,17 +1,32 @@
+"use client"
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import NavItem from './mainNav/navItem';
+
 export default function Header() {
+
+    const navItems = [
+        { title: 'Home', link: '/' },
+        { title: 'To-Do', link: '/to-do' },
+        { title: 'Events', link: '/events' },
+        { title: 'Quotes', link: '/quotes' },
+        { title: 'Images', link: '/images' },
+        { title: 'Login', link: '/login' },
+    ];
+
     return (
         <header className="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-indigo-600">
             <h1>Motivational To-Do</h1>
             <nav aria-label="Main Menu">
                 <ul>
-                    <li><a href="#"><h4>Home</h4></a></li>
-                    <li><a href="#"><h4>To-Do</h4></a></li>
-                    <li><a href="#"><h4>Events</h4></a></li>
-                    <li><a href="#"><h4>Quotes</h4></a></li>
-                    <li><a href="#"><h4>Images</h4></a></li>
-                    <li><a href="#"><h4>Login</h4></a></li>
+                    {navItems.map((item, index) => (
+                        <NavItem key={index} title={item.title} link={item.link} active={usePathname() === item.link} />
+                    ))}
                 </ul>
             </nav>
+            <h2>Pathname: {usePathname()}</h2>
         </header>
-    )
+    );
 };
