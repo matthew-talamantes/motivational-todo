50904fb2ac8df027fd7da1a569c64f7ed341d81e
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2023-09-13 10:11:17 -0400

    TS Typo fix

--- frontend/src/components/mainNav/navItem.tsx
+++ frontend/src/components/mainNav/navItem.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-export default function NavItem({ title, link, active = false }: { title: string, link: string, active: boolean }) {
+export default function NavItem({ title, link, active = false }: { title: string, link: string, active?: boolean }) {
     return (
         <li className="nav-item">
             <Link href={link} aria-current={active ? 'page' : false}><h4>{title}</h4></Link>
