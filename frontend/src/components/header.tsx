2c60b767385927e1f1c07e68cb1a01b9dfd913b7
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2023-09-13 19:04:24 -0400

    Fix text-color bug

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import React from 'react';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import NavItem from './mainNav/navItem';
 
 export default function Header({ textColor = 'black' }: { textColor?: string }) {
+
+    const [textColorState, setTextColorState] = React.useState(textColor);
 
     const navItems = [
         { title: 'Home', link: '/' },
@@ -18,23 +22,18 @@ export default function Header({ textColor = 'black' }: { textColor?: string }) 
 
     const getHeaderClass = (textColor: string) => {
         let colorClass: string;
-        switch (textColor) {
-            case 'white': {
-                colorClass = 'text-white';
-            }
-            case 'black': {
-                colorClass = 'text-black';
-            }
-
-            default: {
-                colorClass = 'text-black';
-            }
+        if (textColor === 'white') {
+            colorClass = 'text-white';
+        } else if (textColor === 'black') {
+            colorClass = 'text-black';
+        } else {
+            colorClass = 'text-black';
         }
         return `flex justify-between items-center py-4 px-6 border-b-4 border-indigo-600 ${colorClass}`;
     };
 
     return (
-        <header className={getHeaderClass(textColor)}>
+        <header className={getHeaderClass(textColorState)}>
             <h1>Motivational To-Do</h1>
             <nav aria-label="Main Menu">
                 <ul>
@@ -43,7 +42,6 @@ export default function Header({ textColor = 'black' }: { textColor?: string }) 
                     ))}
                 </ul>
             </nav>
-            <h2>Pathname: {usePathname()}</h2>
         </header>
     );
 };
