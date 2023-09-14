4d39ede13b909da62001abc53d1ecf1f88551c84
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2023-09-14 15:01:20 -0400

    Update menu styles

--- frontend/src/components/mainNav/navItem.tsx
+++ frontend/src/components/mainNav/navItem.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function NavItem({ title, link, active = false }: { title: string, link: string, active?: boolean }) {
     return (
         <li className="nav-item">
-            <Link href={link} aria-current={active ? 'page' : false}><h4>{title}</h4></Link>
+            <Link href={link} aria-current={active ? 'page' : false}><h5>{title}</h5></Link>
         </li>
     )
 }
