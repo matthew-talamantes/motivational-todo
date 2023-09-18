41feab997cd68b9b0329dbabb07aa9f720d8a787
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2023-09-18 10:12:18 -0400

    Add quotes

--- frontend/src/components/homePage/featuredQuote.tsx
+++ frontend/src/components/homePage/featuredQuote.tsx
@@ -6,7 +6,7 @@ export default function FeaturedQuote({ quote }: { quote: Quote }) {
 
     return (
         <article id="featuredQuote" className={styles.featuredQuote}>
-            <h2 className={`quote quote-lg ${styles.quote}`}>{quote.text}</h2>
+            <h2 className={`quote quote-lg ${styles.quote}`}>"{quote.text}"</h2>
             <p className={`attribution attribution-lg ${styles.attribution}`}>- {quote.author}</p>
         </article>
     );
