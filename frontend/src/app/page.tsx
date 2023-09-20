bd520c93abfd69724dfd20eda157fdcfe5bfd534
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2023-09-20 10:37:35 -0400

    Fix todo widget display

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Quote, ToDoItem, ToDoList } from "@/types";
+import { Quote } from "@/types";
 import React from "react";
 
 import BackgroundImage from "@/components/homePage/backgroundImg";
@@ -6,7 +6,6 @@ import Header from "@/components/header";
 import CurrentDate from "@/components/homePage/currentDate";
 import FeaturedQuote from "@/components/homePage/featuredQuote";
 import WidgetGroup from "@/components/homePage/widgetGroup";
-import ToDoWidget from "@/components/homePage/widgets/toDoWidget";
 
 import styles from "./styles.module.scss";
 
@@ -40,37 +39,7 @@ const getQuote = async () => {
   return quote;
 };
 
-const getFavoriteList = async (url: string) => {
-  const response = await fetch(url);
-  const rawFavoriteList = await response.json();
-  const favoriteList: ToDoList = {
-    id: rawFavoriteList.id, name: rawFavoriteList.name, createdBy: rawFavoriteList.createdBy, createdAt: new Date(rawFavoriteList.createdAt), items: rawFavoriteList.items.map((item) => (
-      { id: item.id, list_id: item.list_id, text: item.text, done: item.done, createdBy: item.createdBy, createdAt: new Date(item.createdAt) } as ToDoItem)
-    )
-  };
-  return favoriteList;
-};
 
-const addTodoItem = async (url: string, item: ToDoItem) => { };
-const editTodoItem = async (url: string, item: ToDoItem) => { };
-
-const getWidgets = async () => {
-  let widgets: object[] = [];
-  const todoUrl: string = "http://localhost:3001/favorite-list/1";
-  const favoriteList: ToDoList = await getFavoriteList(todoUrl).then(list => {
-    return list;
-  });
-  const toDoWidget: object = {
-    widgetType: ToDoWidget,
-    widgetProps: {
-      toDoItems: favoriteList.items,
-      addFunction: addTodoItem,
-      editFunction: editTodoItem
-    }
-  };
-  widgets.push(toDoWidget);
-  return widgets;
-};
 
 export default async function Home() {
 
@@ -85,7 +54,7 @@ export default async function Home() {
         <Header textColor='white' />
         <CurrentDate />
         <FeaturedQuote quote={quote} />
-        <WidgetGroup widgets={getWidgets()} />
+        <WidgetGroup />
       </div>
     </main>
   )
--- frontend/src/components/homePage/widgetGroup.tsx
+++ frontend/src/components/homePage/widgetGroup.tsx
@@ -1,10 +1,53 @@
-import React, { FC } from "react";
+"use client";
+
+import React from "react";
+import { ToDoItem, ToDoList } from "@/types";
 import WidgetWrapper from "./widgets/widgetWrapper";
+import ToDoWidget from "@/components/homePage/widgets/toDoWidget";
 
 import styles from "./widgetGroupStyles.module.scss";
 
-export default function WidgetGroup({ widgets }: { widgets: object[] | Promise<object[]> }) {
 
+const addTodoItem = async (url: string, item: ToDoItem) => {
+};
+const editTodoItem = async (url: string, item: ToDoItem) => {
+};
+
+const getFavoriteList = async (url: string) => {
+    const response = await fetch(url);
+    const rawFavoriteList = await response.json();
+    const favoriteList: ToDoList = {
+        id: rawFavoriteList.id, name: rawFavoriteList.name, createdBy: rawFavoriteList.createdBy, createdAt: new Date(rawFavoriteList.createdAt), items: rawFavoriteList.items.map((item) => (
+            { id: item.id, list_id: item.list_id, text: item.text, done: item.done, createdBy: item.createdBy, createdAt: new Date(item.createdAt) } as ToDoItem)
+        )
+    };
+    return favoriteList;
+};
+
+
+const getWidgets = async () => {
+    let widgets: object[] = [];
+    const todoUrl: string = "http://localhost:3001/favorite-list/1";
+    const favoriteList: ToDoList = await getFavoriteList(todoUrl).then(list => {
+        return list;
+    });
+    const toDoWidget: object = {
+        widgetType: ToDoWidget,
+        widgetProps: {
+            toDoItems: favoriteList.items,
+            addFunction: addTodoItem,
+            editFunction: editTodoItem
+        }
+    };
+    widgets.push(toDoWidget);
+    return widgets;
+};
+
+export default async function WidgetGroup() {
+
+    const widgets: object[] = await getWidgets().then(widgets => {
+        return widgets;
+    });
     const widgetRender = (widget: object, key: number) => {
         const Widget: React.JSX.Element = widget.widgetType;
         const props = widget.widgetProps;
